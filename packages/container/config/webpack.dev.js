5fa39d612223da874647b8f8055047b5b3bbe0ec
Vito_Chu <Vito_Chu@wistron.com>
2023-01-26 12:10:05 +0800

    Initial commit

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,30 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "conatiner",
+      remotes: {
+        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+      },
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+// devConfig寫在後面代表如果有重複的設定，會用devConfig內容複寫
+module.exports = merge(commonConfig, devConfig);
